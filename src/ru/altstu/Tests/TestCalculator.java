318a432bb99b1a4513295b5219bf62e09a310a20
Sergey <serg_soft@mail.ru>
2017-03-27 15:36:40 +0700

    Create test calss for Calculator

---
+++ src/ru/altstu/Tests/TestCalculator.java
@@ -0,0 +1,13 @@
+package ru.altstu.Tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by sergey on 27/03/2017.
+ */
+public class TestCalculator {
+
+
+
+}
