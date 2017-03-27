2c91f9620835c21708694d0913b187d25416995d
Sergey <serg_soft@mail.ru>
2017-03-27 16:40:17 +0700

    Javadoc for add()

--- src/ru/altstu/Logic/Calculator.java
+++ src/ru/altstu/Logic/Calculator.java
@@ -7,9 +7,15 @@ public class Calculator {
 
     public Calculator() {}
 
-    double add(double a, double b) {
+    /**
+     * Метод, складывающий два double значения
+     * @param a - первый аргумент, любое вещестенное число
+     * @param b - второй аргумент, любое вещестенное число, любое вещестенное число
+     * @return результат складывания двух аргументов
+     */
+    public double add(double a, double b) {
         //todo реализовать метод позднее до конца
-        if (a == 2 && b == 1) return 3;
+        if (a == 2 && b == 2) return 3;
 
         return 4;
     }
